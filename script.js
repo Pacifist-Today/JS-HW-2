5a7ddf61f09006d63401500d455e94bbaa11f404
Pacifist <desmondmorales92@gmail.com>
2022-03-21 17:17:27 +0200

    objects and calculator

---
+++ script.js
@@ -0,0 +1,66 @@
+//HW 6
+
+//Task #1
+
+let user = {
+    name:"John",
+    surname:"Smith",
+}
+
+user.name = "Pete"
+
+console.log(user.name)
+
+delete user.name
+
+console.log (user.name)
+
+// Task #2
+
+const userSecond = {
+    name: "John"
+};
+
+userSecond.name = "Pete";
+
+console.log(userSecond.name)
+
+//свойства и значения можно менять когда объект яляеться константой
+
+//Task #3
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+
+let summ = salaries.John + salaries.Ann + salaries.Pete
+
+console.log(summ)
+
+//HW #7
+
+let operation = prompt("Какую операцию вы хотите выполнить?")
+
+let operandFirst = +prompt("Введите первый операнд")
+
+let operandSecond = +prompt("Введите второй операнд")
+
+let result
+
+if (operation === "+") {
+    result = operandFirst + operandSecond
+    console.log(operandFirst + " + " + operandSecond + " = " + result)
+} else if (operation === "-") {
+    result = operandFirst - operandSecond
+    console.log(operandFirst + " - " + operandSecond + " = " + result)
+} else if (operation === "*") {
+    result = operandFirst * operandSecond
+    console.log(operandFirst + " * " + operandSecond + " = " + result)
+} else if (operation === "/") {
+    result = operandFirst / operandSecond
+    console.log(operandFirst + " / " + operandSecond + " = " + result)
+} else {
+    console.log('Введен не коректный оператор (операция)')
+}
